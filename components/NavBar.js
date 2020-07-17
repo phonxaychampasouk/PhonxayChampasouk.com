b5486f12efdaf1fa28a6dd5488f1ec4b69f34c4f
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-07-16 17:02:55 -0700

    In the browser, the index page now shows a blue box serving as a header, a white box with a message serving as content, and a red box serving as the navigation bar. With a layout in place, I am ready to create more pages with a consistent structure.

---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+const navBarStyle = {
+    backgroundColor: "red",
+    color: "white",
+    width: "100%",
+    height: "60px"
+  };
+  
+  const NavBar = () => (
+    <div className="NavBar" style={navBarStyle}>
+      NAVBAR
+    </div>
+  );
+  
+  export default NavBar;
+  
