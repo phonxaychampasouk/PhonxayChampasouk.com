7e74a07002204ceeff8c618a9924b02a78f6905e
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-07-24 01:10:29 -0700

    Added a responsive immage to Index.js.

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae6805d977b3adf6efeb";
+/******/ 	var hotCurrentHash = "bd1f63e27df780bae3c8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 import Layout from '../components/Layout';
 
-const Index = () => <Layout>Welcome to PhonxayChampasouk.com!</Layout>;
+const Index = () => {
 
+    return (
+<Layout>
+    <div className="container">
+<img className="image" src="/me_resided.jpg" />
+</div>
+
+</Layout>
+    );
+}
 export default Index;
