2bbe9ce9626dcab19157fa80edf5c5e32df6f9e7
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-08-21 11:47:55 -0700

    Added about me to landing page

--- components/Feature.js
+++ components/Feature.js
@@ -1,42 +1,74 @@
 import '../styles/Feature.scss';
-// import HeroBg from './featureComponents/HeroBg';
-// import HeroInner from './featureComponents/HeroInner';
 
 function Feature() {
-  const styling = {
-    backgroundImage: 'url("/me.jpg")',
-    width: '100%',
-    height: '100%',
-  };
   return (
-    <header id="home" style={styling}>
-      <div className="row banner">
-        <div className="banner-text">
-          <h1 className="responsive-headline">
-            Im
-            Phonxay Champasouk
-            .
-          </h1>
-          <h3>
-            Im a
-            resident of Seattle Washington
-            {' '}
-            based
-            <span>Software Developer</span>
-            .
-            I did this and I want do it more
-            .
-          </h3>
-          <hr />
-          <ul className="social" />
+    <>
+      <img src="/me.jpg" className="landing-page-image" alt="tetst" />
+      <header id="home">
+        <div className="row banner">
+          <div className="banner-text">
+            <h1 className="responsive-headline">
+              Im
+              Phonxay Champasouk
+              .
+            </h1>
+            <h3>
+              Im a
+              resident of Seattle Washington
+              {' '}
+              based
+              <span>Software Developer</span>
+              .
+              I did this and I want do it more
+              .
+            </h3>
+            <hr />
+            <ul className="social" />
+            <section id="about">
+              <div id="background-image">
+                <div className="row">
+                  <div className="three columns">
+                    <img className="profile-pic" src="/moose.jpeg" alt="Tim Baker Profile Pic" />
+                  </div>
+                  <div className="nine columns main-col">
+                    <h2>About Me</h2>
+                    <p>Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.</p>
+                    <h2>About Me</h2>
+                    <p>Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.</p>
+                    <h2>About Me</h2>
+                    <p>Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.Hi, I like to do a lot of things.</p>
+                    <div className="row">
+                      <div className="columns contact-details">
+                        <h2>Contact Details</h2>
+                        <p className="address">
+                          <span>Phonxay</span>
+                          <br />
+                          <span>
+                            3509 South Thistle Street
+
+                            <br />
+                            Seattle, WA
+                            {' '}
+                            FL,
+                            ,
+                            {' '}
+                            98118
+                          </span>
+                          <br />
+                          <span>7274833740</span>
+                          <br />
+                          <span>phonxay.champasouk@gmail.com</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
-
-      <p className="scrolldown">
-        <a className="smoothscroll" href="#about"><i className="icon-down-circle" /></a>
-      </p>
-
-    </header>
+      </header>
+    </>
 
   );
 }
