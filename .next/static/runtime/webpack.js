163c39173f802f71131e91105704e184e4b02b7d
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-08-21 11:51:11 -0700

    Remove unnecessary packages

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b81c08206c0be4c6967";
+/******/ 	var hotCurrentHash = "515c615701ff5d82e97b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 // import Layout from '../components/Layout';
+import Head from 'next/head';
 import Feature from '../components/Feature';
 import Slider from '../components/Slider';
+import Resume from '../components/Resume';
 import '../styles/index.scss';
 
 const Index = () => (
-  <div className="main-container">
-    <Feature />
-    <Slider />
-  </div>
-
+  <>
+    <Head>
+      <link
+        async
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/semantic-ui@${props.versions.sui}/dist/semantic.min.css"
+      />
+      <script
+        async
+        src="//cdn.jsdelivr.net/npm/semantic-ui@${props.versions.sui}/dist/semantic.min.js"
+      />
+    </Head>
+    <div className="main-container">
+      <Feature />
+      <Slider />
+      <Resume />
+    </div>
+  </>
 );
 export default Index;
