ea71bb8e23754be6307a507f1cf3dd54c4f32cc5
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-08-22 09:23:02 -0700

    Created Component

--- components/sliderComponents/Cards.js
+++ components/sliderComponents/Cards.js
@@ -1,63 +1,40 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Reveal } from 'semantic-ui-react';
 
 const Cards = () => (
   <>
-    <div className="card-image">
-      <Image
-        src="/mia.jpg"
-        as="a"
-        size="medium"
-        href="http://google.com"
-        target="_blank"
-        rounded
-        className="image"
-      />
-    </div>
-    <div className="card-image">
-      <Image
-        src="/smallimage.jpg"
-        as="a"
-        size="medium"
-        href="http://google.com"
-        target="_blank"
-        rounded
-        className="image"
-      />
-    </div>
-    <div className="card-image">
-      <Image
-        src="/highseth.jpg"
-        as="a"
-        size="medium"
-        href="http://google.com"
-        target="_blank"
-        rounded
-        className="image"
-      />
-    </div>
-    <div className="card-image">
-      <Image
-        src="/seth.jpg"
-        as="a"
-        size="medium"
-        href="http://google.com"
-        target="_blank"
-        rounded
-        className="image"
-      />
-    </div>
-    <div className="card-image">
-      <Image
-        src="/snake.jpg"
-        as="a"
-        size="medium"
-        href="http://google.com"
-        target="_blank"
-        rounded
-        className="image"
-      />
-    </div>
+    <Reveal animated="fade">
+      <Reveal.Content visible>
+        <Image src="/me.jpg" size="medium" />
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Image src="/moose.jpeg" size="medium" />
+      </Reveal.Content>
+    </Reveal>
+    <Reveal animated="fade">
+      <Reveal.Content visible>
+        <Image src="/mia.jpg" size="medium" />
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Image src="/pat.jpg" size="medium" />
+      </Reveal.Content>
+    </Reveal>
+    <Reveal animated="fade">
+      <Reveal.Content visible>
+        <Image src="/seth.jpg" size="medium" />
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Image src="highSeth.jpg" size="medium" />
+      </Reveal.Content>
+    </Reveal>
+    <Reveal animated="fade">
+      <Reveal.Content visible>
+        <Image src="/snake.jpg" size="medium" />
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Image src="/lake.jpg" size="medium" />
+      </Reveal.Content>
+    </Reveal>
   </>
 );
 
