c733beca14978c05cad9b9ace169f5b037e8ee27
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-08-25 12:07:59 -0700

    Complete About me and resume  sections, Add Skills Badges

--- components/Feature.js
+++ components/Feature.js
@@ -1,96 +1,120 @@
-import { Button, Icon } from 'semantic-ui-react';
+import { Button, Icon, Image } from 'semantic-ui-react';
 import Resume from './Resume';
 import Skills from './featureComponents/Skills';
 
 function Feature() {
   return (
     <>
-      <img src="/me.jpg" className="landing-page-image" alt="tetst" />
+      <img src="/background.jpg" className="landing-page-image" alt="tetst" />
       <header id="home">
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">
-              >> I'm
-              Phonxay Champasouk.
+              >> I&#39;m
+              Phonxay.
             </h1>
             <h3 id="banner-h3">
-              Im a
-              resident of Seattle Washington
-              {' '}
-              based
-              <span>Software Developer</span>
-              .
-              Let me help you build a beautiful and fast website that will keep your customers engaged.
-              .
+              I'm a <span>Software Developer</span>
+              based in Seattle, Washington .
+              Let me help you build a beautiful
+              and fast website that will keep your customers engaged.
             </h3>
             <hr />
-            <ul className="social" />
-            <section id="about">
-              <div id="background-image">
-                <div className="about-me-row">
-                  <div className="three columns">
-                    <img className="profile-pic" src="/smallimage.jpg" alt="Tim Baker Profile Pic" />
-                  </div>
-                  <div className="nine columns main-col">
-                    <h2>About Me</h2>
-                    <p>
-                      {' '}
-                      Hi! My name is Phonxay! I like to tell people that Phonxay is pronounced like Banzai, but with a "P". Greetings,
-                      I am so happy that you have viewed my site. You can be here for many reasons, such as for photography,
-                      web development, or a social media connection. Any ways, I hope you enjoy the functionalities of my web application. It is still in Development.
-                      I will continue to deploy my application as I complete features.
-                    </p>
-                    <div className="row">
-                      <div className="columns contact-details">
-                        <h2>Contact Details</h2>
-                        <p className="address">
-                          <span>Phonxay</span>
-                          <br />
-                          <span>
-                            3509 South Thistle Street
+          </div>
+          <ul className="social" />
+          <section id="about">
+            <div id="background-image">
+              <div className="about-me-row">
+                <div className="avatar">
+                  <Image src="avatar.jpg" size="small" circular bordered />
+                </div>
+                <div className="nine columns main-col">
+                  <h2>About Me</h2>
+                  <p>
+                    {' '}
+                    Hi! My name is Phonxay! I like to tell people that Phonxay is pronounced like Banzai, but with a "P". Greetings,
+                    I am so happy that you have viewed my site. You can be here for many reasons, such as for photography,
+                    web development, or a social media connection. Any ways, I hope you enjoy the functionalities of my web application. It is still in Development.
+                    I will continue to deploy my application as I complete features.
+                  </p>
+                  <div className="row">
+                    <div className="columns contact-details">
+                      <h2>Contact Details</h2>
+                      <p className="address">
+                        <span>Phonxay</span>
+                        <br />
+                        <span>
+                          3509 South Thistle Street
 
-                            <br />
-                            Seattle, WA
-                            {' '}
-                            FL,
-                            ,
-                            {' '}
-                            98118
-                          </span>
                           <br />
-                          <span>7274833740</span>
-                          <br />
-                          <span>phonxay.champasouk@gmail.com</span>
-                        </p>
-                        <div>
-                          <Button color="github">
+                          Seattle, WA
+                          {' '}
+                          FL,
+                          ,
+                          {' '}
+                          98118
+                        </span>
+                        <br />
+                        <span>7274833740</span>
+                        <br />
+                        <span>phonxay.champasouk@gmail.com</span>
+                      </p>
+                      <div id="social-buttons">
+                        <Button animated="vertical" color="linkedin">
+                          <Button.Content visible>
+                            {' '}
                             <Icon name="github" />
                             {' '}
-                            github
-                          </Button>
-                          <Button color="linkedin">
+                            Github
+                          </Button.Content>
+                          <Button.Content hidden>
+                            <Icon name="github" />
+                            {' '}
+                            Connect
+                          </Button.Content>
+                        </Button>
+                        <Button animated="vertical" color="linkedin">
+                          <Button.Content visible>
+                            {' '}
                             <Icon name="linkedin" />
                             {' '}
                             LinkedIn
-                          </Button>
-                          <Button color="instagram">
+                          </Button.Content>
+                          <Button.Content hidden>
+                            <Icon name="linkedin" />
+                            {' '}
+                            Connect
+                          </Button.Content>
+                        </Button>
+                        <Button animated="vertical" color="instagram">
+                          <Button.Content visible>
+                            {' '}
                             <Icon name="instagram" />
                             {' '}
                             Instagram
-                          </Button>
-                        </div>
+                          </Button.Content>
+                          <Button.Content hidden>
+                            <Icon name="instagram" />
+                            {' '}
+                            Connect
+                          </Button.Content>
+                        </Button>
                       </div>
-                      <div className="columns download">
-                        <Button content="Download Resume" />
-                      </div>
+                    </div>
+                    <div className="download-button">
+                      <Button animated>
+                        <Button.Content visible>Resume</Button.Content>
+                        <Button.Content hidden>
+                          Download
+                        </Button.Content>
+                      </Button>
                     </div>
                   </div>
                 </div>
               </div>
-            </section>
-          </div>
+            </div>
+          </section>
           <Resume />
-
         </div>
       </header>
     </>
--- components/Resume.js
+++ components/Resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Skills from './featureComponents/Skills';
 
 const Resume = () => {
   const object = {
@@ -6,30 +7,35 @@ const Resume = () => {
       skillmessage: 'Here you can create a short write-up of your skills to show off to employers',
       education: [
         {
-          school: 'Galvinize: Hack Reactor',
+          school: 'Hack Reactor',
           degree: 'Seattle Campus',
           graduated: 'May 2020',
           description: 'Hack Reactor is an immersive full-stack development program that teaches students to master advanced HTML and CSS, front-end JavaScript frameworks with React, server development with Node, and databases with SQL and Mongo during 800 hours of instruction, projects, and hands-on work.',
         },
         {
           school: 'Microsoft Software and Systems Academy',
-          degree: 'What did you study 101',
+          degree: 'Fort Bragg',
           graduated: 'Nov 2019',
           description: 'The MSSA teaches transitioning service members the fundamentals of C# programming while building local, server based and cloud-stored applications. Concepts include database programming, data integration and manipulation, C# application programming with ASP.net MVC and application development in Azure.',
+        }, {
+          school: 'Seattle Central College',
+          degree: 'AA in Computer Science',
+          graduated: 'Expected: Jul 2021',
+          description: 'Current Student',
         },
       ],
       work: [
         {
           company: 'US Army',
-          title: 'Senior UX God ',
+          title: 'Infantry Sargeant',
           years: 'Sept 2014 - Sept 2019',
-          description: 'I worked close',
+          description: 'I worked closely with allied forces across our North Atlantic Treaty Organization partners. While I was stationed in Grafenwohr, Germany (Bavaria), I had the opportunity to build relations with amazing people from all backgrounds. Promoted to Sargeant for technical expertise. Created training regimens for use for 1000 + people in Land Navigation.',
         },
         {
           company: 'Best Buy Mobile',
-          title: 'Junior bug fixer',
-          years: 'March 2007 - February 2010',
-          description: 'Describe work, special projects, notable achievements, what technologies you have been working with, and anything else that would be useful for an employer to know.',
+          title: 'Sales Solution Specialist',
+          years: 'Jul 2012 - Nov 2013',
+          description: 'Provided superior customer service which resulted in high level of sales and customer retention. Taught customers how to utilize products and demonstrated new features upon software updates. Recommended, selected and helped locate merchandise based on customer needs and desires',
         },
       ],
     },
@@ -63,43 +69,40 @@ const Resume = () => {
 
   return (
     <section id="resume">
+      <div id="resume-background-image">
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1 id="banner-title"><span>Education</span></h1>
+          </div>
 
-      <div className="row education">
-        <div className="three columns header-col">
-          <h1 id="banner-title"><span>Education</span></h1>
-        </div>
-
-        <div className="nine columns main-col">
-          <div className="row item">
-            <div className="twelve columns">
-              {education}
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="row work">
 
-      <div className="row work">
+          <div className="three columns header-col">
+            <h1 id="banner-title"><span>Work</span></h1>
+          </div>
 
-        <div className="three columns header-col">
-          <h1 id="banner-title"><span>Work</span></h1>
+          <div className="nine columns main-col">
+            {work}
+          </div>
         </div>
 
-        <div className="nine columns main-col">
-          {work}
-        </div>
-      </div>
+        <div className="row skill">
 
-      <div className="row skill">
+          <div className="three columns header-col">
+            <h1 id="banner-title"><span>Skills</span></h1>
+          </div>
 
-        <div className="three columns header-col">
-          <h1 id="banner-title"><span>Skills</span></h1>
-        </div>
-
-        <div className="nine columns main-col">
-
-          <p>
-            {skillmessage}
-          </p>
+          <div className="skills-container">
+            <Skills />
+          </div>
         </div>
       </div>
     </section>
