11ed25faee077ceba4190be534a47e6f8b74aaad
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-08-29 13:00:14 -0700

    Create button to toggle Bot to display

--- components/PhotoPortfolio.js
+++ components/PhotoPortfolio.js
@@ -1,19 +1,53 @@
+import React, { Component } from 'react';
+
 import {
-  Image, Container, Button, Icon,
+  Grid, Segment, Label, Container, Button, Icon,
 } from 'semantic-ui-react';
 
-const PhotoPortfolio = () => (
-  <>
-    <img src="/background3.png" className="portfolio-background" alt="Good Looking Moose" />
-    <header id="photo-display">
-      <div className="row banner">
-        <Button icon>
-          <Icon name="arrow circle right" />
-        </Button>
-      </div>
-    </header>
+// eslint-disable-next-line react/prefer-stateless-function
+class PhotoPortfolio extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayMooseBot: false,
+    };
+    this.toggleMooseBot = this.toggleMooseBot.bind(this);
+  }
 
-  </>
-);
+  toggleMooseBot= () => {
+    const { displayMooseBot } = this.state;
+
+    this.setState({
+      displayMooseBot: !displayMooseBot,
+    });
+  }
+
+  render() {
+const { displayMooseBot } = this.state;
+
+    return (
+      <>
+        <div className="bot-content">
+          <Segment>
+            <Grid>
+              <Grid.Column textAlign="center">
+                <Button>Talk to Moosebot</Button>
+              </Grid.Column>
+            </Grid>
+          </Segment>
+          <div className="portfolio-banner" />
+        </div>
+        <img src="/background3.png"
+        className="portfolio-background"
+        alt="moose head"
+        onClick={()=>this.toggleMooseBot()}
+        />
+        { displayMooseBot ?   <header id="photo-display">teste</header> : <header id="photo-display">failed test</header> }
+      
+      </>
+
+    );
+  }
+}
 
 export default PhotoPortfolio;
