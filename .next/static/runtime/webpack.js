78a91d236909b33a1102e6d80d20c43b3bbe2a2b
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-03 15:44:38 -0700

    Add react photo gallery package to package.json

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ca020b9aa92e55eb20c0";
+/******/ 	var hotCurrentHash = "8e6b41c335c6f8c8cd9c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
