5b1d1d0c91e4c8a64c7c765d71811af83f03cfc4
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-04 14:40:55 -0700

    Send photos users would like to submit to in Swiper Component

---
+++ components/galleryComponents/SwiperForSubmittedPhotos.js
@@ -0,0 +1,8 @@
+const SwiperForSubmittedPhotos = ({displaySubmittedPhotos}) => {
+    return (<div>
+        {displaySubmittedPhotos}
+    </div>
+    );
+}
+
+export default SwiperForSubmittedPhotos;
