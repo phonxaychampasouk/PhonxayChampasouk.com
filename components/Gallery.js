b25ff623f098fb44531d8632501a325dfa6c9d37
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-04 15:14:39 -0700

    Add feature to display photos that users submitted in an image carousel

--- components/Gallery.js
+++ components/Gallery.js
@@ -4,15 +4,26 @@ import Gallery from 'react-photo-gallery';
 
 import photos from './galleryComponents/photos';
 import SelectedImage from './galleryComponents/SelectedImage';
+import SubmittedPhotos from './galleryComponents/SubmittedPhotos';
 
 /* popout the browser and maximize to see more rows! -> */
 
 function BasicRows() {
   const [selectAll, setSelectAll] = useState(false);
+  const [photoCollection, setPhotoCollection] = useState([null]);
+  const [isPhotoDisplayed, setIsPhotoDisplayed] = useState(false);
+
 
   const toggleSelectAll = () => {
     setSelectAll(!selectAll);
   };
+  const handleOnSubmit = (e) => {
+    setPhotoCollection(photoCollection.map((photo) => parseInt(photo, 10)));
+    setIsPhotoDisplayed(true);
+    console.log('photoCollections should be all ints', photoCollection);
+    e.preventDefault();
+  };
+
   const imageRenderer = useCallback(
     ({
       index, left, top, key, photo,
@@ -25,15 +36,24 @@ function BasicRows() {
         photo={photo}
         left={left}
         top={top}
+        setPhotoCollection={setPhotoCollection}
       />
     ),
     [selectAll],
   );
   return (
-    <Gallery
-      photos={photos}
-      renderImage={imageRenderer}
-    />
+    <>
+      <SubmittedPhotos
+        photoCollection={photoCollection}
+        handleOnSubmit={handleOnSubmit}
+        isPhotoDisplayed={isPhotoDisplayed}
+
+      />
+      <Gallery
+        photos={photos}
+        renderImage={imageRenderer}
+      />
+    </>
   );
 }
 export default BasicRows;
