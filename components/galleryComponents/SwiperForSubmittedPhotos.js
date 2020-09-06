1e672855eb4f82183195703237fe73b728220e8e
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-06 11:29:31 -0700

    Add coverflow card component

--- components/galleryComponents/SwiperForSubmittedPhotos.js
+++ components/galleryComponents/SwiperForSubmittedPhotos.js
@@ -1,8 +1,25 @@
-const SwiperForSubmittedPhotos = ({displaySubmittedPhotos}) => {
-    return (<div>
-        {displaySubmittedPhotos}
+import Coverflow from 'react-coverflow';
+
+const SwiperForSubmittedPhotos = ({ displaySubmittedPhotos, initialPhoto }) => (
+  <Coverflow
+    width="100%"
+    height={480}
+    displayQuantityOfSide={2}
+    navigation={false}
+    enableHeading={false}
+  >
+    <div
+      role="menuitem"
+      tabIndex="0"
+    >
+      <img
+        src="[image/path/please_change]"
+        alt="title or description"
+        style={{ display: 'block', width: '100%' }}
+      />
     </div>
-    );
-}
+    {displaySubmittedPhotos}
+  </Coverflow>
+);
 
 export default SwiperForSubmittedPhotos;
