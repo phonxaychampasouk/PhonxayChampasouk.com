149797e92a8740f0cfc4c6f91850e14f4e8621f5
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-09 10:58:04 -0700

    Update package.json

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "63c9da6dbb6c37ab343f";
+/******/ 	var hotCurrentHash = "fd43732eb6ce9e99d12f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
