650eb55dfaa571895b32563c69b3221b6e60d525
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-09 19:55:05 -0700

    bConfig for web app

--- index.js
+++
@@ -1,3 +0,0 @@
-const express = require('express');
-
-const server = express(); const options = { index: 'index.html' }; server.use('/', express.static('/home/site/wwwroot', options)); server.listen(process.env.PORT);
