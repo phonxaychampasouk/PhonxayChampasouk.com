2c8641dfc47029940fb3c30353fd6b07800eecdf
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-10 15:19:23 -0700

    Add styles to _app

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import '../styles/PhotoPortfolio.scss';
 import '../styles/SelectedImage.scss';
 import '../styles/_plugin-react-slick.scss';
 import '../styles/SubmittedPhotos.scss';
+import '../styles/Values.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
