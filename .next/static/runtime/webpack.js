f0697f5e17ccf13a7788749f4a41bc433c0ece78
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-10 18:01:21 -0700

    webpack updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3d6cc68c5b4b8547d6ed";
+/******/ 	var hotCurrentHash = "162b48649470356cea36";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
