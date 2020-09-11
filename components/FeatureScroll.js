bc91c1c03b73d83eccd9e3cba01f77dfcd84deeb
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-11 13:33:28 -0700

    Removed unneccessary code

--- components/FeatureScroll.js
+++ components/FeatureScroll.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,7 +11,12 @@ import Container from '@material-ui/core/Container';
 import Fab from '@material-ui/core/Fab';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
+import { Chat } from '@material-ui/icons';
 import Feature from './Feature';
+
+import ChatBot from './ChatBot';
+
+import KEYS from '../keys';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +28,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function ScrollTop(props) {
+  const [mooseBotDisplay, setMooseBotDisplay] = useState(false);
+
   const { children, window } = props;
   const classes = useStyles();
   // Note that you normally won't need to set the window ref as useScrollTrigger
@@ -34,20 +41,30 @@ function ScrollTop(props) {
     threshold: 100,
   });
 
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+  const handleClick = ({ target }) => {
+    const identifier = target.getAttribute('class');
+    if (identifier === 'MuiSvgIcon-root') {
+      const anchor = (target.ownerDocument || document).querySelector('#back-to-top-anchor');
 
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      if (anchor) {
+        anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    } else {
+      setMooseBotDisplay(!mooseBotDisplay);
     }
   };
 
   return (
-    <Zoom in={trigger}>
-      <div onClick={handleClick} role="presentation" className={classes.root}>
-        {children}
-      </div>
-    </Zoom>
+    <>
+      <Zoom in={trigger}>
+        <div onClick={handleClick} role="presentation" className={classes.root} name="to-top-button">
+          <Fab color="secondary" size="large" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+          <ChatBot handleClick={handleClick} mooseBotDisplay={mooseBotDisplay} />
+        </div>
+      </Zoom>
+    </>
   );
 }
 
@@ -60,16 +77,14 @@ ScrollTop.propTypes = {
   window: PropTypes.func,
 };
 
-export default function BackToTop(props) {
+export default function FeatureScroll(props) {
   return (
     <>
       <CssBaseline />
       <Toolbar id="back-to-top-anchor" />
-        <Feature />
-      <ScrollTop {...props}>
-        <Fab color="secondary" size="large" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
+      <Feature />
+      <ScrollTop>
+        <ChatBot />
       </ScrollTop>
     </>
 
