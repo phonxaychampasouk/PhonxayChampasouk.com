e70a2ee17fde3eb56dcda4615371af7d1f89872c
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-11 13:36:33 -0700

    Move onScroll feature and remove uneccessary code

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -1,64 +1,87 @@
-import React, { Component } from 'react';
-import {
-  Grid, Segment, Button,
-} from 'semantic-ui-react';
-//import KEYS from '../keys';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+import Button from '@material-ui/core/Button';
+import { Chat } from '@material-ui/icons';
+import Feature from './Feature';
 
-class PhotoPortfolio extends Component {
-  constructor() {
-    super();
-    this.state = {
-      displayMooseBot: false,
-    };
-    this.toggleMooseBot = this.toggleMooseBot.bind(this);
-  }
+import KEYS from '../keys';
 
-  toggleMooseBot= () => {
-    const { displayMooseBot } = this.state;
-    this.setState({
-      displayMooseBot: !displayMooseBot,
-    });
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+    zIndex: '101',
+  },
+}));
 
-  render() {
-    const { displayMooseBot } = this.state;
+function OpenChatBot({
+  children, window, handleClick, mooseBotDisplay,
+}) {
+  const classes = useStyles();
 
-    return (
-      <>
-        <div className="bot-content">
-          <Segment>
-            <Grid>
-              <Grid.Column textAlign="center">
-                <Button onClick={() => this.toggleMooseBot()}>
-                  Talk to Moosebot
-                </Button>
-              </Grid.Column>
-            </Grid>
-            {
-              displayMooseBot
-                ? (
-                  <header id="bot-content">
-                    <iframe
-                      src={`https://webchat.botframework.com/embed/chat-bot-resume?s=${KEYS.mychatbot}`}
-                      style={{ minWidth: '400px', width: '100%', minHeight: '500px' }}
-                    />
-                  </header>
-                )
-                : <header id="photo-display">failed test</header>
-                }
-          </Segment>
-          <div className="portfolio-banner" />
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  return (
+    <>
+      <Zoom in={trigger}>
+        <div
+          className="moosebot-click-container"
+          onClick={handleClick}
+          name="moosebot"
+        >
+          {children}
         </div>
-        <img
-          src="https://phonxaydocuments.blob.core.windows.net/phonxayblob/background3.png"
-          className="portfolio-background"
-          alt="moose head"
-          onClick={() => this.toggleMooseBot()}
-        />
-      </>
-
-    );
-  }
+      </Zoom>
+      {
+mooseBotDisplay ? (
+  <header id="bot-content">
+    <iframe
+      title="moosebot"
+      src={`https://webchat.botframework.com/embed/chat-bot-resume?s=${KEYS}`}
+      style={{ minWidth: '400px', width: '100%', minHeight: '500px' }}
+    />
+  </header>
+)
+  : <header id="photo-display">failed test</header>
+    }
+    </>
+  );
 }
 
-export default PhotoPortfolio;
+const ChatBot = ({ handleClick, mooseBotDisplay }) => (
+  <>
+    {
+mooseBotDisplay ? (
+  <header id="bot-content">
+    <iframe
+      title="moosebot"
+      src={`https://webchat.botframework.com/embed/chat-bot-resume?s=${KEYS}`}
+      style={{ minWidth: '400px', width: '100%', minHeight: '500px' }}
+    />
+  </header>
+)
+  : (
+    <Button variant="contained" color="primary" onClick={handleClick}>
+      Talk to MooseBot
+    </Button>
+  )
+    }
+  </>
+);
+export default ChatBot;
