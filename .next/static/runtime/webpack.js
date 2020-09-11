592cca75de9be17187fe7d3e38c7cbb886f8d1fc
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-10 23:26:06 -0700

    add particles-bg dependecy

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "162b48649470356cea36";
+/******/ 	var hotCurrentHash = "61f76823babe58ef04dd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
