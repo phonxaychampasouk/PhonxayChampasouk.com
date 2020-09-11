34a21176361f8563b28cef1b9290191d50dfa3df
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-11 11:03:40 -0700

    Add background image from Blob Storage

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fa54c3247368dc3ad1b2";
+/******/ 	var hotCurrentHash = "63edf3bd8d2b9226a9d7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
