fd3e8cdc104664989c9613786126af86292d1619
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-15 14:22:09 -0700

    Deploying to azure

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e7c2798af85642f0f806";
+/******/ 	var hotCurrentHash = "08f3ba7e140fbed4ff3a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
