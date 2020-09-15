925acfdb201f5e282a012b0189eac9045cfe77dd
Phonxay Champasouk <phonxay.champasouk@outlook.com>
2020-09-14 23:08:31 -0700

    Configure next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   exportTrailingSlash: true,
-  exportPathMap() {
+  exportPathMap: function() {
     return {
-      '/': { page: '/' },
+      '/': { page: '/' }
     };
-  },
+  }
 };
